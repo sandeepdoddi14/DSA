70ff1d01956e74bf4b721c7be6e3e400ca74b6b7
yetendrakumar <yetendrakumar.tpa@jiostar.com>
2025-10-16 23:16:45 +0530

    add commit

--- src/dsa/Arrays.java
+++ src/dsa/Arrays.java
@@ -1,15 +1,15 @@
 package dsa;
 
+import java.util.HashMap;
 import java.util.PriorityQueue;
 
 public class Arrays {
 
     public static void main(String[] args) {
-        int[] nums = {2, 2};
-        System.out.println(findErrorNums(nums));
+        int[] nums = {8};
+        System.out.println((minimumOperations(nums)));
 
     }
-
 
 
     public boolean checkString(String s) {
@@ -78,5 +78,68 @@ public class Arrays {
                 minHeap.remove(minHeap.peek());
         }
         return minHeap.peek();
+    }
+
+    public static int duplicateNumbersXOR(int[] nums) {
+        int result = 0;
+        HashMap<Integer, Integer> map = new HashMap<>(); //S(N)
+        for (int num : nums) { //O(N)
+            if (map.containsKey(num))
+                map.put(num, map.get(num) + 1);
+            else
+                map.put(num, 1);
+        }
+        for (int key : map.keySet()) { //O(N)
+            if (map.get(key) == 2) {
+                result = result ^ key;
+            }
+        }
+
+        return result;
+    }
+
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int sum = 0;
+        int maxSum = 0;
+
+        for (int num : nums) {
+            if (num == 1) {
+                sum++;
+                maxSum = Math.max(maxSum, sum);
+            } else {
+                sum = 0;
+            }
+        }
+        return maxSum;
+
+    }
+
+
+    public static int minOperations(int[] nums, int k) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int reduceNo=sum%k;
+        if(reduceNo==0 && sum > k){
+            return reduceNo;
+        }
+        else if(sum<k)
+            return sum;
+
+        return 0;
+    }
+
+    public static int minimumOperations(int[] nums) {
+        int result = 0;
+        for(int num : nums){
+            int r=num%3;
+            if(r<=3/2)
+                result+=r;
+            else
+                result = result + (3-r);
+        }
+        return result;
     }
 }
